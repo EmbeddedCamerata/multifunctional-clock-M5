cd04d58b8b92f6ee5716ec0ff14c0e36d85e229d
KafCoppelia <k740677208@gmail.com>
2023-06-12 00:29:23 +0800

    🧰 chore: rename main `.ino`

---
+++ multifunctional-clock-M5.ino
@@ -0,0 +1,36 @@
+#include "include/utils.h"
+
+SysTypeDef UserSystem = {
+    .SysPage = PAGE_UNKNOWN,
+    .SysEvents = NULL
+};
+
+TaskHandle_t xhandle_page_update = NULL;
+TaskHandle_t xhandle_buttons_update = NULL;
+SemaphoreHandle_t lcd_draw_sem = NULL;
+
+void setup()
+{
+    M5.begin();
+
+    // 1. Create semaphores
+    if ((lcd_draw_sem = xSemaphoreCreateMutex()) == NULL) {
+        Serial.println("Semaphore created failed!");
+        return;
+    }
+
+    if ((UserSystem.SysEvents = xEventGroupCreate()) == NULL) {
+        Serial.println("Event group created failed!");
+        return;
+    }
+
+    SystemInit(&UserSystem);
+
+    xTaskCreate(PageUpdate, "PageUpdate", 1024*2, (void*)&UserSystem, 4, &xhandle_page_update);
+    xTaskCreate(ButtonsUpdate, "ButtonsUpdate", 1024*2, (void*)&UserSystem, 5, &xhandle_buttons_update);
+}
+
+void loop()
+{
+
+}
